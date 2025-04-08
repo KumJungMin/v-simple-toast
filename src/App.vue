fd30a758b28ee97948812565a3276ee0ea1d0a8a
KumJungMin <rmawjdals@naver.com>
2025-04-08 22:44:38 +0900

    feat: update sample code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div></div>
+  <div>
+    <Toast mode="stack" />
+    <!-- <Toast mode="sequential" /> -->
+
+    <button @click="showToast">토스트 열기</button>
+    <button @click="showToast">토스트 열기2</button>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useToast } from './@lib/toast/composables/useToast';
+
+
+function showToast(): void {
+  useToast().add('안녕하세요! 토스트 메시지입니다.');
+}
+</script>
